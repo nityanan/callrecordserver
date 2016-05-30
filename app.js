66073d432205b54d5161352fa914b48e4e6961d6
nitya <nitya.nand@daffodilsw.com>
2016-05-30 20:53:39 +0530

    ok

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var routes = require('./index');
 
 var app = express();
 
-//process.on('uncaughtException', function (mainErr) {
-//    var msg = "Error at " + new Date() + "\n" + mainErr + "\n-------------------------------------------\n";
-//    console.log("err>>>>>>" + mainErr + mainErr.stack);
-//    var fs = require("fs");
-//    var fd = fs.openSync('./err.txt', 'a')
-//    fs.writeSync(fd, msg);
-//    fs.closeSync(fd)
-//});
+process.on('uncaughtException', function (mainErr) {
+    var msg = "Error at " + new Date() + "\n" + mainErr + "\n-------------------------------------------\n";
+    console.log("err>>>>>>" + mainErr + mainErr.stack);
+    var fs = require("fs");
+    var fd = fs.openSync('./err.txt', 'a')
+    fs.writeSync(fd, msg);
+    fs.closeSync(fd)
+});
 app.use('/', routes);
 
 // catch 404 and forward to error handler
